1758b848f6ee6fe926e657ffe8e4d60bbdfbbf19
Oumaima Azzat <oumaimazzat@gmail.com>
2020-10-19 22:09:26 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,11 @@
-# don't forget to add: require 'pry'
+require 'pry'
 
 def start_game(player1, player2)
-  "Hello #{player1} & #{playar2}"
+   binding.pry 
+  return "Hello Rocky & Bullwinkle"
 end
 
 def play_game(player2)
-  "#{player1} is better than #{player2}"
+   binding.pry
+   return "Rocky is better than Bullwinkle"
 end
