c4279c38fda839ece38aaa0404240b323157457e
Oumaima Azzat <oumaimazzat@gmail.com>
2020-10-19 22:37:49 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,7 +5,7 @@ def start_game(player1, player2)
   return "Hello Rocky & Bullwinkle"
 end
 
-def play_game(player2, player1)
+def play_game(player1, player2)
    binding.pry
    return "#{player1} is better than #{player2}"
 end
